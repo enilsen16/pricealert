ae96ce54176ceaba5f113c402fa29920fb17771b
Erik Nilsen <enilsen16@live.com>
2018-08-13 13:53:44 -0700

    Remove all .pyc files

--- pricealert/settings.py
+++ pricealert/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pricealertweb.apps.PricealertwebConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- pricealertweb/apps.py
+++ pricealertweb/apps.py
@@ -3,6 +3,5 @@ from __future__ import unicode_literals
 
 from django.apps import AppConfig
 
-
 class PricealertwebConfig(AppConfig):
     name = 'pricealertweb'
