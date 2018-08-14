f40920bc890db8b0dc474683b4cb3461c2fd0daf
Erik Nilsen <enilsen16@live.com>
2018-08-13 17:28:24 -0700

    Add marketdata table

--- pricealertweb/models.py
+++ pricealertweb/models.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import datetime
+from djmoney.models.fields import MoneyField
 
-# Create your models here.
+class MarketData(models.Model):
+    created_at = models.DateTimeField(default=datetime.datetime.now)
+    type = models.CharField(max_length=200)
+    price = MoneyField(max_digits=19, decimal_places=2, default_currency='USD')
