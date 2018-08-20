4e36874324e9c11bf5ecee69b337e399b6f18dea
Erik Nilsen <enilsen16@live.com>
2018-08-20 14:20:51 -0700

    Calculate avergage price in 10 minute period

--- pricealert/tasks.py
+++ pricealert/tasks.py
@@ -1,7 +1,15 @@
+from .models import MarketData, Alert
 from celery.schedules import crontab
+from datetime import datetime
+from django.db.models import Avg
 from pricealert.celery import app
-from .models import Alert
 
 @app.task
 def calculate_average_price():
     # This is pretty naive but it'll work for our purposes
+    return MarketData.objects.exclude(
+        created_at__lte=datetime.datetime.now() - datetime.timedelta(minutes=10)
+    ).aggregate(
+        Avg('price')
+    )
+
