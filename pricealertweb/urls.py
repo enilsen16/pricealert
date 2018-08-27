fa3060e629baff5d25ce760c0566a50f21166457
Erik Nilsen <enilsen16@live.com>
2018-08-27 15:13:49 -0700

    improve some style

--- pricealertweb/urls.py
+++ pricealertweb/urls.py
@@ -4,6 +4,5 @@ from pricealertweb.alert import views
 
 urlpatterns = [
     url(r'^$', login_required(views.AlertView.as_view())),
-    url(r'^new/', login_required(views.CreateAlertView.as_view())),
-    url(r'^edit/(?P<pk>[\w-]+)$', login_required(views.UpdateAlertView.as_view())),
+    url(r'^new/', login_required(views.CreateAlertView.as_view()), name="alert_new"),
 ]
